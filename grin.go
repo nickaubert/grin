a842b40bff2e643fcd742597d20f62b17d6d2b18
Nick Aubert <nickaubert@gmail.com>
2013-07-07 13:57:43 -0400

    started to draw well walls

--- grin.go
+++ grin.go
@@ -1,61 +1,56 @@
 package main
 
+// http://tetrisconcept.net/wiki/Tetris_Guideline
+
 import "code.google.com/p/goncurses"
 import "fmt"
 
 func main() {
+
 	stdscr, _ := goncurses.Init()
 	defer goncurses.End()
 
-    row, col := stdscr.Maxyx()
-	stdscr.Print("Hello, World %d %d !!!" , row , col )
-
-    // msg := "Just a string "
-    // stdscr.MovePrint(row/2, (col-len(msg))/2, msg)
-
-    // stdscr.MovePrint(row-3, 0, "This screen has %d rows and %d columns. ", row, col)
-    // stdscr.MovePrint(row-2, 0, "Try resizing your terminal window and then "+
-    //             "run this program again.")
-    // stdscr.Refresh()
-    // stdscr.GetChar()
-
-	// stdscr.Print("Hello, World %d %d !!!" , row , col )
-	// stdscr.Refresh()
-	// somechar := stdscr.GetChar()
-
-	// stdscr.Refresh()
-    // stdscr.AddChar(goncurses.Character(somechar))
-
-    // if goncurses.Character(somechar) == 'z' {
-    //     stdscr.Print("The Z key was pressed.")
-    // }
-	// stdscr.GetChar()
-
+    term_row, term_col := stdscr.Maxyx()
+	// stdscr.Print("Hello, World %d %d !!!" , term_row , term_col )
 	goncurses.End()
 
-    // this_draw := draw_well
-    // fmt.Println( this_draw )
+    well_width := 10
+    well_depth := 20
 
-    fmt.Println( "this num is ║" )
+    well_border := define_well( well_width , well_depth )
 
-    this_num := draw_well()
-    fmt.Println( this_num )
+    draw_well( well_border , term_row , term_col)
 
-    that_draw := 13
-    fmt.Println( that_draw )
-
-    fmt.Println( 55 )
 }
 
-func draw_well() (s int) {
-    well := make([]byte, 5)
-    for _, c := range well {
-        fmt.Println(c)
+func define_well( well_width , well_depth int ) []byte {
+    // fmt.Println( well_width )
+    // fmt.Println( well_depth )
+    well := make([]byte, well_width)
+    return well
+}
+
+func draw_well( this_well []byte , term_row , term_col int ) {
+    // fmt.Println( term_row )
+    // fmt.Println( term_col )
+
+    var this_line string
+
+    columns_offset := ( ( term_col / 2 ) - ( len(this_well) / 2 ) - 1 )
+    for i := 0 ; i < columns_offset ; i++ { 
+        this_line += " "
+    } 
+
+    // var this_line string
+    this_line += "║"
+    for _, c := range this_well {
+        if c == 0 {
+            this_line += " "
+        } else {
+            this_line += "+"
+        }
     }
-    // fmt.Println( "hohoho" )
-    fmt.Println("whatsup")
-    // fmt.Println( s )
-    s = 12
-    return s
+    this_line += "║"
+    fmt.Println( this_line )
 }
 
