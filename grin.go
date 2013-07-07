402dcb44f95b5d2b0bbb0b32d428e89070204907
Nick Aubert <nickaubert@gmail.com>
2013-07-07 14:04:31 -0400

    old blocks become debris

--- grin.go
+++ grin.go
@@ -13,20 +13,29 @@ func main() {
 
     // define well
     well_dimensions := make( []int , 3 )
-    well_dimensions[0] = 20 // well_depth
-    well_dimensions[1] = 10 // well_width
-    well_dimensions[2] = 5  // vert_headroom
-
-    // starting block location
-    block_location  := make( []int , 2 )
-    block_location[0] = well_dimensions[0]     // block_height
-    block_location[1] = well_dimensions[1] / 2 // block_longitude
+    well_dimensions[0] = 20  // well_depth
+    well_dimensions[1] = 10  // well_width
+    well_dimensions[2] = 5   // vert_headroom
 
     draw_border( stdscr , well_dimensions )
 
+    debris_map := make( [][]int , well_dimensions[0] )
+    for i := 0 ; i < well_dimensions[0] ; i++ {
+        debris_row := make([]int, well_dimensions[1])
+        debris_map[i] = debris_row
+    }
+
+    // starting block location
+    block_location  := make( []int , 2 )
+    new_block( stdscr , well_dimensions , block_location , debris_map )
+    show_stats( stdscr , 1 , "block height  " , block_location[0] )
+    // block_location[0] = well_dimensions[0] - 1 // block_height
+    // block_location[1] = well_dimensions[1] / 2 // block_longitude
+
     for keep_going := true ; keep_going == true ; {
 
-        show_stats( stdscr , block_location[0] )
+        show_stats( stdscr , 1 , "block height  " , block_location[0] )
+        show_stats( stdscr , 2 , "block longtude" , block_location[1] )
 
         // keyboard input
         //  wait to drop time here?
@@ -54,13 +63,16 @@ func main() {
         }
 
         // move block 
-        block_status := move_block( stdscr , well_dimensions , movement , block_location )
+        block_status := move_block( stdscr , well_dimensions , block_location , movement , debris_map )
 
         // new block?
         if block_status == 2 {
-            new_block( well_dimensions , block_location )
+            debris_map[block_location[0]][block_location[1]] = 1
+            nb_ret := new_block( stdscr , well_dimensions , block_location , debris_map )
+            if nb_ret == 2 {
+                keep_going = false
+            }
         }
-
 
     }
 
@@ -69,19 +81,19 @@ func main() {
 }
 
 
-func show_stats( stdscr goncurses.Window , block_height int ) {
+func show_stats( stdscr goncurses.Window , height int , show_text string , show_val int ) {
 
-    bh_status := fmt.Sprintf( "block height: %02d" , block_height )
-    stdscr.MovePrint( 1 , 1  , bh_status )
+    bh_status := fmt.Sprintf( "%s : %02d     " , show_text , show_val )
+    stdscr.MovePrint( height , 1  , bh_status )
 
 }
 
-func move_block( stdscr goncurses.Window , well_dimensions [] int , operation string , block_location []int ) int {
+func move_block( stdscr goncurses.Window , well_dimensions , block_location []int , operation string , debris_map [][]int) int {
 
     block_height    := block_location[0]
     block_longitude := block_location[1]
 
-    blocked := check_collisions( well_dimensions , block_location , operation )
+    blocked := check_collisions( well_dimensions , block_location , debris_map , operation )
 
     if blocked == true {
         if operation == "dropone" {
@@ -93,6 +105,7 @@ func move_block( stdscr goncurses.Window , well_dimensions [] int , operation st
 
     draw_block( stdscr , well_dimensions , "erase" , block_location )
 
+    retstat := 0
     switch {
         case operation == "left" :
             block_longitude--
@@ -101,7 +114,8 @@ func move_block( stdscr goncurses.Window , well_dimensions [] int , operation st
         case operation == "dropone" :
             block_height--
         case operation == "drop" :
-            block_height = 1
+            block_height = sound_depth( block_location , debris_map )
+            retstat = 2
     }
 
     block_location[0] = block_height
@@ -109,25 +123,36 @@ func move_block( stdscr goncurses.Window , well_dimensions [] int , operation st
 
     draw_block( stdscr , well_dimensions , "draw" , block_location )
 
-    return 0
+    return retstat
 
 }
 
-func check_collisions( well_dimensions , block_location []int , operation string ) bool {
+func check_collisions( well_dimensions , block_location []int , debris_map [][]int , operation string ) bool {
 
+    block_height    := block_location[0]
+    block_longitude := block_location[1]
     blocked := false
     switch {
         case operation == "left" :
-            if block_location[1] == 1 {
-                blocked = true
+            switch {
+                case block_longitude == 0 :
+                    blocked = true
+                case debris_map[block_height][block_longitude - 1] == 1 :
+                    blocked = true
             }
         case operation == "right" :
-            if block_location[1] == ( well_dimensions[1] - 1 ) {
-                blocked = true
+            switch {
+                case block_location[1] == ( well_dimensions[1] - 1 ) :
+                    blocked = true
+                case debris_map[block_height][block_longitude + 1] == 1 :
+                    blocked = true
             }
         case operation == "dropone" :
-            if block_location[0] == 1 {
-                blocked = true
+            switch {
+                case block_location[0] == 0 :
+                    blocked = true
+                case debris_map[block_height - 1 ][block_longitude] == 1 :
+                    blocked = true
             }
         case operation == "drop" :
             blocked = false // nothing to do here yet
@@ -136,14 +161,26 @@ func check_collisions( well_dimensions , block_location []int , operation string
     return blocked
 }
 
+func sound_depth( block_location []int , debris_map [][]int ) int {
+
+    block_height    := block_location[0]
+    block_longitude := block_location[1]
+
+    for i := block_height ; i > 0  ; i-- {
+        if debris_map[i][block_longitude] == 1 {
+            return i + 1
+        }
+    }
+
+    return 0
+}
+
 func draw_block( stdscr goncurses.Window , well_dimensions []int , operation string , block_location []int ) bool {
 
     // terminal size
-    // term_row, term_col := stdscr.Maxyx()
     _, term_col := stdscr.Maxyx()
 
-    // well_top    := 5
-    well_left   := ( ( term_col / 2 ) - well_dimensions[1] )
+    well_left := ( ( term_col / 2 ) - well_dimensions[1] )
 
     block_height    := block_location[0]
     block_longitude := block_location[1]
@@ -171,9 +208,9 @@ func draw_border( stdscr goncurses.Window , well_dimensions []int ) {
     well_width    := well_dimensions[1]
     vert_headroom := well_dimensions[2]
 
-    well_left   := ( ( term_col / 2 ) - well_width )
-    well_right  := ( well_left + ( well_width * 2 ) )
-    well_bottom := ( vert_headroom + well_depth )
+    well_left   := ( ( term_col / 2 ) - well_width ) - 2
+    well_right  := well_left + ( well_width * 2 ) + 2
+    well_bottom := vert_headroom + well_depth + 1
 
     // draw sides
     for row_height := vert_headroom ; row_height < well_bottom ; row_height ++ {
@@ -181,7 +218,7 @@ func draw_border( stdscr goncurses.Window , well_dimensions []int ) {
         stdscr.MovePrint( row_height , well_right , "| " )
     }
 
-    for col_loc := well_left ; col_loc <= well_right ; col_loc ++ {
+    for col_loc := ( well_left + 1 ) ; col_loc <= well_right ; col_loc ++ {
         stdscr.MovePrint( well_bottom , col_loc  , "=" )
     }
 
@@ -189,8 +226,33 @@ func draw_border( stdscr goncurses.Window , well_dimensions []int ) {
 
 }
 
-func new_block( well_dimensions , block_location []int ) {
-    block_location[0] = well_dimensions[0]     // block_height
+func new_block( stdscr goncurses.Window , well_dimensions , block_location []int , debris_map [][]int ) int {
+
+    // show_stats( stdscr , 2 , "block ending loc" , block_location[1] )
+    stdscr.GetChar()
+
+    block_location[0] = well_dimensions[0] - 1 // block_height
     block_location[1] = well_dimensions[1] / 2 // block_longitude
+
+    draw_debris( stdscr , well_dimensions , debris_map )
+
+    return 0
 }
 
+func draw_debris( stdscr goncurses.Window , well_dimensions []int , debris_map [][]int ) {
+
+    _, term_col := stdscr.Maxyx()
+    vert_headroom := well_dimensions[2]
+
+    // var well_width int
+    for row := 0 ; row < len( debris_map ) ; row++ {
+        for col := 0 ; col < len( debris_map[row] ) ; col++ {
+            row_loc := vert_headroom + well_dimensions[0] - row
+            col_loc := ( ( term_col / 2 ) - well_dimensions[1] ) + ( col * 2 )
+            if debris_map[row][col] == 1 {
+                stdscr.MovePrint( row_loc , col_loc  , "DD" )
+            }
+        }
+    }
+
+}
