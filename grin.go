e24e8c6b9b59ec48a33f921a9d418ba390937b3f
Nick Aubert <nickaubert@gmail.com>
2013-07-07 13:59:04 -0400

    draw well walls and bottom

--- grin.go
+++ grin.go
@@ -11,7 +11,6 @@ func main() {
 	defer goncurses.End()
 
     term_row, term_col := stdscr.Maxyx()
-	// stdscr.Print("Hello, World %d %d !!!" , term_row , term_col )
 	goncurses.End()
 
     well_width := 10
@@ -21,36 +20,61 @@ func main() {
 
     draw_well( well_border , term_row , term_col)
 
+    // somechar := stdscr.GetChar()
+	// goncurses.End()
+
 }
 
-func define_well( well_width , well_depth int ) []byte {
-    // fmt.Println( well_width )
-    // fmt.Println( well_depth )
-    well := make([]byte, well_width)
+func define_well( well_width , well_depth int ) [][]byte {
+    well := make([][]byte, well_depth)
+    for i := 0 ; i < well_depth ; i++ {
+        well_row := make([]byte, well_width)
+        well[i] = well_row
+    }
     return well
 }
 
-func draw_well( this_well []byte , term_row , term_col int ) {
-    // fmt.Println( term_row )
-    // fmt.Println( term_col )
+func draw_well( this_well [][]byte , term_row , term_col int ) {
 
-    var this_line string
-
-    columns_offset := ( ( term_col / 2 ) - ( len(this_well) / 2 ) - 1 )
-    for i := 0 ; i < columns_offset ; i++ { 
-        this_line += " "
-    } 
-
-    // var this_line string
-    this_line += "║"
-    for _, c := range this_well {
-        if c == 0 {
-            this_line += " "
-        } else {
-            this_line += "+"
-        }
+    // offset from left
+    var line_buffer string
+    columns_offset := ( ( term_col / 2 ) - len(this_well[1]) - 1 )
+    for i := 0 ; i < columns_offset ; i++ {
+        line_buffer += " "
     }
-    this_line += "║"
+
+    // draw well sides
+    var well_width int
+    for _, this_row := range this_well {
+        this_line := line_buffer
+        this_line += "║"
+        for _, c := range this_row {
+            if c == 0 {
+                this_line += "  "
+            } else {
+                this_line += "++" // brick?
+            }
+        }
+
+        this_line += "║"
+        fmt.Println( this_line )
+
+        well_width = len( this_row )
+
+    }
+
+    // draw well bottom
+    this_line := line_buffer
+    this_line += "╩"
+    for i := 0 ; i < well_width ; i++ {
+        this_line += "══"
+    }
+    this_line += "╩"
     fmt.Println( this_line )
+
+    fmt.Println( "hello" )
+    fmt.Printf( "one" )
+    fmt.Printf( "two" )
+
 }
 
